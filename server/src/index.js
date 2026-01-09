ffe80536c4b252c95417be2c0b98f08f08d96f7e
christina <christina.ddding@gmail.com>
2026-01-09 16:44:25 -0500

    document env vars and connect mongodb

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,20 @@
-import express from "express"
-import cors from "cors"
+require("dotenv").config()
+
+const express = require("express")
+const cors = require("cors")
+const connectDB = require("./db")
 
 const app = express()
-const PORT = 5050
-
 app.use(cors())
 app.use(express.json())
 
+connectDB()
+
 app.get("/health", (req, res) => {
-  res.json({ status: "backend alive 🔥" })
+  res.json({ status: "backend + mongo alive 🔥" })
 })
 
-app.listen(PORT, () => {
-  console.log(`Backend running at http://localhost:${PORT}`)
+const PORT = process.env.PORT || 5050
+app.listen(PORT, "127.0.0.1", () => {
+  console.log(`Backend running at http://127.0.0.1:${PORT}`)
 })
