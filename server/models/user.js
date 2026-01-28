55df03d649dcbf089d29fae12e020bc39a532e46
Alicia Ji <its.alicia.ji@gmail.com>
2026-01-27 21:44:02 -0500

    add email to profile page

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: String,
   googleid: String,
+  email: String,
   // statistics
   gamesPlayed: {
     type: Number,
